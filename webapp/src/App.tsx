ff9d26e5508fb434e32b1b67f60f94780147c77a
tyler <tyler@cruftbusters.com>
2022-05-17 17:19:41 -0700

    webapp display connectivity status

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react'
+
+function useBackendStatusText() {
+  const [statusText, setStatusText] = useState('pending')
+
+  useEffect(() => {
+    ;(async () => {
+      while (true) {
+        try {
+          const response = await fetch('https://api.hillshade.io')
+          setStatusText(response.statusText)
+          if (response.status >= 200 && response.status < 300) {
+            break
+          }
+        } catch (error) {
+          setStatusText(`error: ${error}`)
+        }
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+      }
+    })()
+  }, [])
+
+  return statusText
+}
+
 export default function App() {
-  return <div>hello world</div>
+  const statusText = useBackendStatusText()
+  return <div>status: {statusText}</div>
 }
