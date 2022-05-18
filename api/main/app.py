39d8d92b9852680f9f675daa448bdad5ef951759
tyler <tyler@cruftbusters.com>
2022-05-17 17:30:47 -0700

    api returns version at GET /

--- api/main/app.py
+++ api/main/app.py
@@ -1,12 +1,16 @@
+import subprocess
+
 from flask import Flask
 
+version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
-def get():
-  return 'hello world'
+
+def get_version():
+  return version
 
 
 def create_app():
     app = Flask(__name__)
-    app.route('/')(get)
+    app.route('/')(get_version)
     return app
 
