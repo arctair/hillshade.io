d29316e018658da9f833edfc191bab366edd5d75
tyler <tyler@cruftbusters.com>
2022-05-18 13:35:39 -0700

    cartograph draws square with webgl

--- webapp/src/components/Cartograph.tsx
+++ webapp/src/components/Cartograph.tsx
@@ -1,5 +1,7 @@
 import { MutableRefObject, useEffect, useRef, useState } from 'react'
 
+const mat4 = require('gl-mat4')
+
 export default function Cartograph() {
   const ref = useRef() as MutableRefObject<HTMLDivElement>
   const canvasRef = useRef() as MutableRefObject<HTMLCanvasElement>
@@ -12,12 +14,53 @@ export default function Cartograph() {
     canvas.width = div.clientWidth
     canvas.height = Math.floor(div.clientHeight)
 
+    const projectionMatrix = mat4.create()
+    mat4.perspective(
+      projectionMatrix,
+      (45 * Math.PI) / 180,
+      canvas.clientWidth / canvas.clientHeight,
+      0.1,
+      100.0,
+    )
+
+    const modelViewMatrix = mat4.create()
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0])
+
     const gl = canvas.getContext('webgl')
     if (gl === null) {
       return setError('webgl is not supported')
     }
-    gl.clearColor(0.125 / 2, 0.125 / 2, 0.125 / 2, 1)
-    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    try {
+      const shaderProgram = loadShaderProgram(gl)
+
+      bindPositionBuffer(gl, shaderProgram)
+
+      gl.useProgram(shaderProgram)
+
+      gl.uniformMatrix4fv(
+        gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+        false,
+        projectionMatrix,
+      )
+
+      gl.uniformMatrix4fv(
+        gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+        false,
+        modelViewMatrix,
+      )
+
+      gl.clearColor(0.125 / 2, 0.125 / 2, 0.125 / 2, 1)
+      gl.clearDepth(1.0)
+      gl.enable(gl.DEPTH_TEST)
+      gl.depthFunc(gl.LEQUAL)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+
+      setError('')
+    } catch (error) {
+      setError(`shader error: ${error}`)
+    }
   }, [])
 
   return (
@@ -40,4 +83,78 @@ export default function Cartograph() {
       </span>
     </div>
   )
+}
+
+const defaultVertexShaderSource = `
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}`
+
+const defaultFragmentShaderSource = `
+void main() {
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}`
+
+function loadShaderProgram(
+  gl: WebGLRenderingContext,
+  vertexShaderSource: string = defaultVertexShaderSource,
+  fragmentShaderSource: string = defaultFragmentShaderSource,
+) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+  const fragmentShader = loadShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource,
+  )
+
+  const shaderProgram = gl.createProgram()!
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    const shaderInfoLog = gl.getProgramInfoLog(shaderProgram)
+    throw Error(`unable to load the shader program: ${shaderInfoLog}`)
+  }
+
+  return shaderProgram
+}
+
+function loadShader(
+  gl: WebGLRenderingContext,
+  type: number,
+  source: string,
+) {
+  const shader = gl.createShader(type)!
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    const shaderInfoLog = gl.getShaderInfoLog(shader)
+    gl.deleteShader(shader)
+    return Error(`unable to load the shader: ${shaderInfoLog}`)
+  }
+  return shader
+}
+
+function bindPositionBuffer(
+  gl: WebGLRenderingContext,
+  shaderProgram: WebGLProgram,
+  positions: Float32Array = new Float32Array([
+    1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0,
+  ]),
+) {
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
+  const attribLocation = gl.getAttribLocation(
+    shaderProgram,
+    'aVertexPosition',
+  )
+  gl.vertexAttribPointer(attribLocation, 2, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(attribLocation)
 }
