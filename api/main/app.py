5ebb51176e34610831c65814681511c471d05977
tyler <tyler@cruftbusters.com>
2022-05-18 17:58:28 -0700

    api proxies google satellite tiles

--- api/main/app.py
+++ api/main/app.py
@@ -1,6 +1,7 @@
 import subprocess
 
-from flask import Flask
+import requests as requests
+from flask import Flask, request, Response
 
 version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
@@ -9,8 +10,14 @@ def get_version():
   return version
 
 
-def create_app():
-    app = Flask(__name__)
-    app.route('/')(get_version)
-    return app
+def get_google_tile(query_string):
+  url = f'https://mt0.google.com/vt/{query_string}'
+  response = requests.get(url)
+  return Response(response.content, mimetype=response.headers['Content-Type'])
 
+
+def create_app():
+  app = Flask(__name__)
+  app.route('/')(get_version)
+  app.route('/mt0.google.com/vt/<query_string>')(get_google_tile)
+  return app
