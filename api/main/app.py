ce8eec4a7b07da7b8eed250216dd05bf2c01a2d6
tyler <tyler@cruftbusters.com>
2022-05-18 18:06:13 -0700

    Revert "api proxies google satellite tiles"
    This reverts commit 5ebb51176e34610831c65814681511c471d05977.

--- api/main/app.py
+++ api/main/app.py
@@ -1,7 +1,6 @@
 import subprocess
 
-import requests as requests
-from flask import Flask, request, Response
+from flask import Flask
 
 version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
@@ -10,14 +9,8 @@ def get_version():
   return version
 
 
-def get_google_tile(query_string):
-  url = f'https://mt0.google.com/vt/{query_string}'
-  response = requests.get(url)
-  return Response(response.content, mimetype=response.headers['Content-Type'])
-
-
 def create_app():
-  app = Flask(__name__)
-  app.route('/')(get_version)
-  app.route('/mt0.google.com/vt/<query_string>')(get_google_tile)
-  return app
+    app = Flask(__name__)
+    app.route('/')(get_version)
+    return app
+
