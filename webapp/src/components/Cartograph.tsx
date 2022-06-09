0e369709bb779f61bd5a512584270cec331b6c05
tyler <tyler@cruftbusters.com>
2022-06-09 10:25:59 -0700

    draw square to screen

--- webapp/src/components/Cartograph.tsx
+++ webapp/src/components/Cartograph.tsx
@@ -8,16 +8,13 @@ import {
 import MapControls from './MapControls'
 import { defaultViewState, viewStateReducer } from './ViewState'
 
-// eslint-disable-next-line
 const mat4 = require('gl-mat4')
 
 export default function Cartograph() {
   const ref = useRef() as MutableRefObject<HTMLDivElement>
   const canvasRef = useRef() as MutableRefObject<HTMLCanvasElement>
 
-  // eslint-disable-next-line
   const [error, setError] = useState('')
-  // eslint-disable-next-line
   const [viewState, dispatch] = useReducer(
     viewStateReducer,
     defaultViewState,
@@ -26,12 +23,13 @@ export default function Cartograph() {
   useEffect(() => {
     canvasRef.current.width = viewState.mapSize[0]
     canvasRef.current.height = viewState.mapSize[1]
-  }, [viewState.mapSize])
-
-  useEffect(() => {
-    const cartographWebGL = new CartographWebGL(canvasRef.current)
+    if (viewState.mapSize[0] <= 1) return
+    const cartographWebGL = new CartographWebGL(
+      canvasRef.current,
+      setError,
+    )
     return () => cartographWebGL.teardown()
-  }, [])
+  }, [viewState.mapSize])
 
   return (
     <div style={{ height: '100%', position: 'relative' }} ref={ref}>
@@ -66,15 +64,131 @@ export default function Cartograph() {
 }
 
 class CartographWebGL {
-  constructor(canvas: HTMLCanvasElement) {
+  constructor(
+    canvas: HTMLCanvasElement,
+    onError: (message: string) => void,
+  ) {
     const gl = canvas.getContext('webgl')
     if (gl === null) {
-      throw Error(
+      onError(
         `Unable to initialize WebGL. Your browser or machine may not support it.`,
       )
+      return
     }
-    gl.clearColor(0, 0, 0, 1)
-    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER)!
+    gl.shaderSource(
+      vertexShader,
+      ` attribute vec4 aVertexPosition;
+
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+
+        void main() {
+          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        }`,
+    )
+    gl.compileShader(vertexShader)
+
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+      const shaderInfoLog = gl.getShaderInfoLog(vertexShader)
+      const message = `An error occurred compiling the shaders: ${shaderInfoLog}`
+      onError(message)
+      gl.deleteShader(vertexShader)
+      return
+    }
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!
+    gl.shaderSource(
+      fragmentShader,
+      ` void main() {
+          gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }`,
+    )
+    gl.compileShader(fragmentShader)
+
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+      const shaderInfoLog = gl.getShaderInfoLog(fragmentShader)
+      const message = `An error occurred compiling the shaders: ${shaderInfoLog}`
+      onError(message)
+      gl.deleteShader(fragmentShader)
+      return
+    }
+
+    const program = gl.createProgram()!
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      const programInfoLog = gl.getProgramInfoLog(program)
+      const message = `Unable to initialize the shader program: ${programInfoLog}`
+      onError(message)
+      return
+    }
+
+    const aVertexPositionLocation = gl.getAttribLocation(
+      program,
+      'aVertexPosition',
+    )
+    const uProjectionMatrixLocation = gl.getUniformLocation(
+      program,
+      'uProjectionMatrix',
+    )
+    const uModelViewMatrix = gl.getUniformLocation(
+      program,
+      'uModelViewMatrix',
+    )
+
+    const positionsBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer)
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(
+        [
+          [1.0, 1.0],
+          [-1.0, 1.0],
+          [1.0, -1.0],
+          [-1.0, -1.0],
+        ].flat(),
+      ),
+      gl.STATIC_DRAW,
+    )
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.clearDepth(1.0)
+    gl.enable(gl.DEPTH_TEST)
+    gl.depthFunc(gl.LEQUAL)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    const projectionMatrix = mat4.create()
+    mat4.perspective(
+      projectionMatrix,
+      (45 * Math.PI) / 180,
+      gl.canvas.clientWidth / gl.canvas.clientHeight,
+      0.1,
+      100.0,
+    )
+
+    const modelViewMatrix = mat4.create()
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0, 0, -6])
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer)
+    gl.vertexAttribPointer(
+      aVertexPositionLocation,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0,
+    )
+    gl.enableVertexAttribArray(aVertexPositionLocation)
+
+    gl.useProgram(program)
+
+    gl.uniformMatrix4fv(uProjectionMatrixLocation, false, projectionMatrix)
+    gl.uniformMatrix4fv(uModelViewMatrix, false, modelViewMatrix)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
   }
 
   teardown() {}
