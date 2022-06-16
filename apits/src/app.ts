03e0fc234dedf3a59a89a20e1107cef8f0c81a62
tyler <tyler@cruftbusters.com>
2022-06-15 18:18:39 -0700

    http hello world

--- apits/src/app.ts
+++ apits/src/app.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+const version = process.env.VERSION || 'dev'
+
+app.get('/', (_: Request, response: Response) =>
+  response.json({ version }),
+)
+
+app.listen(port, () => console.log(`0.0.0.0:${port}`))
