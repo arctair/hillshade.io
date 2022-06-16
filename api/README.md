3cd10b014121b2e06bc07e639f8e984d34f4a99a
tyler <tyler@cruftbusters.com>
2022-06-15 19:34:33 -0700

    run tests against deployment

--- api/README.md
+++ api/README.md
@@ -1,2 +1,17 @@
 # api.hillshade.io
 layouts api will accept new layouts for heightmaps to be attached
+
+## run the app
+```
+npm start
+```
+
+## run the tests
+```
+npm test:watch
+```
+
+## run the tests against deployment
+```
+BASE_URL=https://api.hillshade.io npm test
+```
--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,8 +1,10 @@
 import nodeFetch from 'node-fetch'
 
+const baseURL = process.env.BASE_URL || 'http://localhost:8080'
+
 describe('app', () => {
   test('get version', async () => {
-    const response = await nodeFetch('http://localhost:8080/')
+    const response = await nodeFetch(baseURL)
     const body = (await response.json()) as any
     expect(body.version).toHaveLength(7)
   })
