64c6c2736af2529c54ff956e1879b2d6d3571474
tyler <tyler@cruftbusters.com>
2022-06-26 16:26:48 -0700

    heightmapper - fix yagni deployment by compiling ts to js, nvm install in the right shell, and run compiled js

--- heightmapper/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!(data-uri-to-buffer|fetch-blob|formdata-polyfill|node-fetch))',
-  ],
-}
