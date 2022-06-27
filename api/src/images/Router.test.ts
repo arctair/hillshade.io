4d454c78b2547f8f001939477a4db13364a1e839
tyler <tyler@cruftbusters.com>
2022-06-26 18:44:58 -0700

    api - refactor - massage test names

--- api/src/images/Router.test.ts
+++ api/src/images/Router.test.ts
@@ -12,49 +12,53 @@ describe('router', () => {
   const app = express()
   app.use('/', Router(store))
 
-  test('post image', async () => {
-    store.create.mockReturnValue(['abcd.jpg', undefined])
-    const response = await request(app)
-      .post('/images')
-      .set('Content-Type', 'image/png')
-    expect(response.status).toEqual(201)
-    expect(response.body).toEqual({ key: 'abcd.jpg' })
-    expect(store.create).toHaveBeenCalledWith('image/png')
+  describe('create image', () => {
+    test('proxy content type up and key down', async () => {
+      store.create.mockReturnValue(['abcd.jpg', undefined])
+      const response = await request(app)
+        .post('/images')
+        .set('Content-Type', 'image/png')
+      expect(response.status).toEqual(201)
+      expect(response.body).toEqual({ key: 'abcd.jpg' })
+      expect(store.create).toHaveBeenCalledWith('image/png')
+    })
+
+    test('proxy content type up and error down with default server error status', async () => {
+      store.create.mockReturnValue([undefined, 'random error'])
+      const response = await request(app)
+        .post('/images')
+        .set('Content-Type', 'image/jpg')
+      expect(response.status).toEqual(500)
+      expect(response.body).toEqual({ error: 'random error' })
+      expect(store.create).toHaveBeenCalledWith('image/jpg')
+    })
+
+    test('proxy content type up and no content type error down with bad request status', async () => {
+      store.create.mockReturnValue([undefined, errNoContentType])
+      const response = await request(app).post('/images')
+      expect(response.status).toEqual(400)
+      expect(response.body).toEqual({ error: errNoContentType })
+      expect(store.create).toHaveBeenCalledWith(undefined)
+    })
   })
 
-  test('post image error', async () => {
-    store.create.mockReturnValue([undefined, 'random error'])
-    const response = await request(app)
-      .post('/images')
-      .set('Content-Type', 'image/jpg')
-    expect(response.status).toEqual(500)
-    expect(response.body).toEqual({ error: 'random error' })
-    expect(store.create).toHaveBeenCalledWith('image/jpg')
-  })
+  describe('get image', () => {
+    test('proxy key up and content type down', async () => {
+      store.get.mockReturnValue(['image/png', undefined])
+      const response = await request(app).get('/images/abcd.jpg')
+      expect(response.status).toEqual(200)
+      expect(response.headers['content-type']).toBe(
+        'image/png; charset=utf-8',
+      )
+      expect(store.get).toHaveBeenCalledWith('abcd.jpg')
+    })
 
-  test('post image with no content type results in bad request', async () => {
-    store.create.mockReturnValue([undefined, errNoContentType])
-    const response = await request(app).post('/images')
-    expect(response.status).toEqual(400)
-    expect(response.body).toEqual({ error: errNoContentType })
-    expect(store.create).toHaveBeenCalledWith(undefined)
-  })
-
-  test('get image', async () => {
-    store.get.mockReturnValue(['image/png', undefined])
-    const response = await request(app).get('/images/abcd.jpg')
-    expect(response.status).toEqual(200)
-    expect(response.headers['content-type']).toBe(
-      'image/png; charset=utf-8',
-    )
-    expect(store.get).toHaveBeenCalledWith('abcd.jpg')
-  })
-
-  test('get image error', async () => {
-    store.get.mockReturnValue([undefined, 'boom'])
-    const response = await request(app).get('/images/abcd.png')
-    expect(response.status).toEqual(500)
-    expect(response.body).toEqual({ error: 'boom' })
-    expect(store.get).toHaveBeenCalledWith('abcd.png')
+    test('proxy key up and error down with default server error status', async () => {
+      store.get.mockReturnValue([undefined, 'boom'])
+      const response = await request(app).get('/images/abcd.png')
+      expect(response.status).toEqual(500)
+      expect(response.body).toEqual({ error: 'boom' })
+      expect(store.get).toHaveBeenCalledWith('abcd.png')
+    })
   })
 })
