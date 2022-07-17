6f59b295eff353861bdf06b47d94b23ee0f00b2c
tyler <tyler@cruftbusters.com>
2022-07-17 11:45:31 -0700

    add world-to-screen resolution to layout summary

--- webapp/src/components/LayoutSummary.tsx
+++ webapp/src/components/LayoutSummary.tsx
@@ -4,15 +4,26 @@ interface LayoutSummaryProps {
   layout: Layout
 }
 export function LayoutSummary({ layout }: LayoutSummaryProps) {
+  const [horizontalResolution, verticalResolution] = selectResolution(
+    layout,
+  ).map((v) => v.toFixed(2))
   return (
     <>
       <div>width: {layout.size[0]}</div>
       <div>height: {layout.size[1]}</div>
       <div>extent: [ {layout.extent.join(' ')} ] (EPSG:3857)</div>
+      <div>
+        resolution: {horizontalResolution}x{verticalResolution}{' '}
+        meters/pixel
+      </div>
     </>
   )
 }
 
+const selectResolution = (layout: Layout) => [
+  (layout.extent[2] - layout.extent[0]) / layout.size[0],
+  (layout.extent[3] - layout.extent[1]) / layout.size[1],
+]
 interface KeyedLayoutSummaryProps {
   layout?: KeyedLayout
 }
