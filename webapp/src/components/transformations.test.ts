d865a9dfc5e89feccbe091c0bf940da1e477a0de
tyler <tyler@cruftbusters.com>
2022-07-26 17:48:40 -0700

    fixup! webapp - stop taking it out on the git logs

--- webapp/src/components/transformations.test.ts
+++ webapp/src/components/transformations.test.ts
@@ -1,4 +1,8 @@
-import { EPSG_3857_TO_EPSG_4326 } from './transformations'
+import {
+  EPSG_3857_TO_EPSG_4326,
+  MAX_3857_X,
+  MAX_3857_Y,
+} from './transformations'
 
 describe('3857 to 4326', () => {
   test('transform origin', () => {
@@ -7,15 +11,12 @@ describe('3857 to 4326', () => {
     expect(actual).toStrictEqual(expected)
   })
   test('transform north east', () => {
-    const actual = EPSG_3857_TO_EPSG_4326([20026376.39, 20048966.1])
+    const actual = EPSG_3857_TO_EPSG_4326([MAX_3857_X, MAX_3857_Y])
     const expected = [180, 85.05112877946931]
     expect(actual).toStrictEqual(expected)
   })
   test('transform half north east', () => {
-    const actual = EPSG_3857_TO_EPSG_4326([
-      20026376.39 / 2,
-      20048966.1 / 2,
-    ])
+    const actual = EPSG_3857_TO_EPSG_4326([MAX_3857_X / 2, MAX_3857_Y / 2])
     const expected = [90, 66.51326044233275]
     expect(actual).toStrictEqual(expected)
   })
